83fc61c8ee0b4cc40601eaa453aaa7462de4f27d
John Bae <john21225@gmail.com>
2022-01-12 16:06:04 -0800

    Changes

--- app/src/App.js
+++ app/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { extent, scaleLinear, format } from "d3";
 import { useData } from "./hooks/useData";
 import "./App.css";
@@ -7,8 +7,10 @@ import "./App.css";
 import AxisBottom from "./components/AxisBottom";
 import AxisLeft from "./components/AxisLeft";
 import CircleMark from "./components/CircleMark";
+import Dropdown from "./components/Dropdown";
 
 const App = () => {
+	// Values for visual
 	const width = 1200;
 	const height = 500;
 	const margin = { top: 30, right: 30, bottom: 80, left: 250 };
@@ -18,11 +20,21 @@ const App = () => {
 	const xAxisLabelOffset = 70;
 	const yAxisLabelOffset = -70;
 
+	// Values for data
 	const xAxisLabel = "Sepal Length";
 	const yAxisLabel = "Sepal Width";
 	const xValue = (d) => d.sepal_length;
 	const yValue = (d) => d.sepal_width;
+	const initialXAttribute = "petal_length";
+	const initialYAttribute = "petal_width";
+	console.log(data.columns);
+	const attributes = [{}];
 
+	// States for axis
+	const [xAttribute, setXAttribute] = useState(initialXAttribute);
+	const [yAttribute, setYAttribute] = useState(initialYAttribute);
+
+	// Formatting for axis
 	const siFormat = format(".2s");
 	const xAxisTickFormat = (tickValue) => siFormat(tickValue).replace("G", "B");
 
@@ -41,41 +53,50 @@ const App = () => {
 		.nice();
 
 	return (
-		<svg width={width} height={height}>
-			<g transform={`translate(${margin.left}, ${margin.top})`}>
-				<AxisBottom
-					xScale={xScale}
-					innerHeight={innerHeight}
-					tickFormat={xAxisTickFormat}
-				/>
-				<text
-					className="axis-label"
-					x={yAxisLabelOffset}
-					y={innerHeight / 2}
-					style={{ textAnchor: "middle" }}
-					transform={`rotate(-90, ${yAxisLabelOffset}, ${innerHeight / 2})`}
-				>
-					{yAxisLabel}
-				</text>
-				<AxisLeft yScale={yScale} innerWidth={innerWidth} />
-				<CircleMark
-					data={data}
-					xScale={xScale}
-					yScale={yScale}
-					xValue={xValue}
-					yValue={yValue}
-					toolTipFormat={xAxisTickFormat}
-				/>
-				<text
-					className="axis-label"
-					x={innerWidth / 2}
-					y={innerHeight + xAxisLabelOffset}
-					style={{ textAnchor: "middle" }}
-				>
-					{xAxisLabel}
-				</text>
-			</g>
-		</svg>
+		<>
+			<label for="x-select">X:</label>
+			<Dropdown
+				options={attributes}
+				id="x-select"
+				selectedValue={xAttribute}
+				onSelectedValueChange={setXAttribute}
+			/>
+			<svg width={width} height={height}>
+				<g transform={`translate(${margin.left}, ${margin.top})`}>
+					<AxisBottom
+						xScale={xScale}
+						innerHeight={innerHeight}
+						tickFormat={xAxisTickFormat}
+					/>
+					<text
+						className="axis-label"
+						x={yAxisLabelOffset}
+						y={innerHeight / 2}
+						style={{ textAnchor: "middle" }}
+						transform={`rotate(-90, ${yAxisLabelOffset}, ${innerHeight / 2})`}
+					>
+						{yAxisLabel}
+					</text>
+					<AxisLeft yScale={yScale} innerWidth={innerWidth} />
+					<CircleMark
+						data={data}
+						xScale={xScale}
+						yScale={yScale}
+						xValue={xValue}
+						yValue={yValue}
+						toolTipFormat={xAxisTickFormat}
+					/>
+					<text
+						className="axis-label"
+						x={innerWidth / 2}
+						y={innerHeight + xAxisLabelOffset}
+						style={{ textAnchor: "middle" }}
+					>
+						{xAxisLabel}
+					</text>
+				</g>
+			</svg>
+		</>
 	);
 };
 
