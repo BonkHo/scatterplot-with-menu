b8732d4c493fd4f2791e1ff178c456fa8373d92d
John Bae <john21225@gmail.com>
2022-01-17 18:11:27 -0800

    Styling menus

--- app/src/App.js
+++ app/src/App.js
@@ -68,18 +68,24 @@ const App = () => {
 
 	return (
 		<>
-			<span className="dropdown-label">X:</span>
-			<ReactDropdown
-				options={attributes}
-				selectedValue={xAttribute}
-				onChange={({ value }) => setXAttribute(value)}
-			/>
-			<span className="dropdown-label">Y:</span>
-			<ReactDropdown
-				options={attributes}
-				selectedValue={yAttribute}
-				onChange={({ value }) => setYAttribute(value)}
-			/>
+			<div className="menus-container">
+				<div className="single-dropdown">
+					<span className="dropdown-label">X:</span>
+					<ReactDropdown
+						options={attributes}
+						value={xAttribute}
+						onChange={({ value }) => setXAttribute(value)}
+					/>
+				</div>
+				<div className="single-dropdown">
+					<span className="dropdown-label">Y:</span>
+					<ReactDropdown
+						options={attributes}
+						value={yAttribute}
+						onChange={({ value }) => setYAttribute(value)}
+					/>
+				</div>
+			</div>
 			<svg width={width} height={height}>
 				<g transform={`translate(${margin.left}, ${margin.top})`}>
 					<AxisBottom
